c50e395a2ac339c537becbc6d44ba16f72b8ae04
ksss <co000ri@gmail.com>
2020-12-08 15:00:17 +0900

    Add spec for Filesystem#copy

--- spec/mini_paperclip/storage/filesystem_spec.rb
+++ spec/mini_paperclip/storage/filesystem_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe MiniPaperclip::Storage::Filesystem do
     expect(FileUtils).to receive(:cp).with('file.png', %r{spec/temp/records/images/.*.png})
     filesystem.write(:original, file)
   end
+
+  it "#copy" do
+    from_record = Record.new(image_file_name: 'image.png')
+    to_record = Record.new(image_file_name: 'image.png')
+    from_attachment = MiniPaperclip::Attachment.new(from_record, :image, { storage: :filesystem })
+    to_attachment = MiniPaperclip::Attachment.new(to_record, :image)
+
+    file = Rack::Test::UploadedFile.new "spec/paperclip.jpg", 'image/jpeg'
+    from_attachment.assign(file)
+    from_attachment.process_and_store
+    filesystem = MiniPaperclip::Storage::Filesystem.new(to_attachment, MiniPaperclip.config)
+    expect(filesystem.exists?(:original)).to eq(false)
+    filesystem.copy(:original, from_attachment)
+    expect(filesystem.exists?(:original)).to eq(true)
+  end
 end
