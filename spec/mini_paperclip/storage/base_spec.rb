b195fc8a3d2e75be519eaea7c147e856194cd40b
ksss <co000ri@gmail.com>
2020-12-24 12:50:05 +0900

    Should use dup

--- spec/mini_paperclip/storage/base_spec.rb
+++ spec/mini_paperclip/storage/base_spec.rb
@@ -3,14 +3,14 @@ RSpec.describe MiniPaperclip::Storage::Base do
 
   it "#path_for with missing file" do
     attachment = MiniPaperclip::Attachment.new(record, :image)
-    config = MiniPaperclip.config
+    config = MiniPaperclip.config.dup
     base = MiniPaperclip::Storage::Base.new(attachment, config)
     expect(base.path_for(:original)).to eq('images/original/missing.png')
   end
 
   it "#path_for with empty file" do
     attachment = MiniPaperclip::Attachment.new(record, :image)
-    config = MiniPaperclip.config
+    config = MiniPaperclip.config.dup
     base = MiniPaperclip::Storage::Base.new(attachment, config)
     record.image_file_name = ""
     expect(base.path_for(:original)).to eq('records/images/25a80ba6aa8c48f17ea32fc7935fab633e807238.')
@@ -18,7 +18,7 @@ RSpec.describe MiniPaperclip::Storage::Base do
 
   it "#path_for with file" do
     attachment = MiniPaperclip::Attachment.new(record, :image)
-    config = MiniPaperclip.config
+    config = MiniPaperclip.config.dup
     base = MiniPaperclip::Storage::Base.new(attachment, config)
     record.image_file_name = "test.png"
     expect(base.path_for(:original)).to eq('records/images/25a80ba6aa8c48f17ea32fc7935fab633e807238.png')
